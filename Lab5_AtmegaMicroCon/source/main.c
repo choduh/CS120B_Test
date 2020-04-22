79d3257508f8c24e194ca3e4d7380c1a9842b5d3
choduh <choduh1995@gmail.com>
2020-04-22 02:03:56 -0700

    needs fix

--- Lab5_AtmegaMicroCon/source/main.c
+++ Lab5_AtmegaMicroCon/source/main.c
@@ -1,7 +1,7 @@
-/*	Author: lab
+/*	Author: magui070
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section:023
+ *	Assignment: Lab #5  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,9 +14,48 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
 
+	unsigned char tank;
     /* Insert your solution below */
     while (1) {
+
+	    unsigned char key = PINA & 0x10;
+	    unsigned char seat = PINA & 0x20;
+	    unsigned char seatBelt = PINA & 0x40;
+
+	    tank = 0x00;
+	    tank = PINA & 0x0F;
+	    if(tank <= 4){
+		    PORTC = 0x40;
+		}
+	    else{
+		    PORTC = 0x00;
+	    }
+	    if(tank == 0x01 || tank == 0x02){
+		    PORTC = PORTC | 0x20;
+	    }
+	    else if(tank == 3 || tank == 4){
+		   PORTC = PORTC | 0x30;
+	    }
+	    else if(tank == 5 || tank == 6){
+			PORTC = PORTC | 0x38;
+	    }
+
+	    else if(tank >= 7 && tank <= 9){
+		    PORTC = PORTC | 0x3C;
+	    }
+	    else if(tank >= 10 && tank <= 12){
+
+		   PORTC = PORTC | 0x3E;
+	    }
+	    else if(tank >= 13 && tank <=15){
+		    PORTC = PORTC | 0x3F;
+	    }
+	    if((key == 0x10) && (seat == 0x20) && (seatBelt == 0x00)){
+		    PORTC = PORTC | 0x80;
+            }	    
 
     }
     return 1;
