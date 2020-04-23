924a58f4159d631a176bda27ce975d7ed941d16a
choduh <choduh1995@gmail.com>
2020-04-23 03:58:32 -0700

    almost done

--- Lab5_AtmegaMicroCon/source/main.c
+++ Lab5_AtmegaMicroCon/source/main.c
@@ -12,110 +12,80 @@
 #include "simAVRHeader.h"
 #endif
 
-enum button{init, increase, decrease, reset, wait1, wait2}state;
+enum button{START, init, seq1,seq2, wait}state;
 
 unsigned char B1;
-unsigned char B2;
-unsigned char tempC;
 
 
 void LED_ON(){
 	B1 = ~PINA & 0x01;
-	B2 = ~PINA & 0x02;
-	//unsigned char tempC = 0x07;
+	
+	
 
 	switch (state){
 
+		case START:
+			PORTC = 0x00;
+			state = seq1;
+			break;
 		case init:
-			if(!B1 && !B2){
+			if(B1 == 0x01){
+				state = seq1;
+			}
+			else if(B1 == 0x02){
+				state = seq2;
+			}
+			else{
+				state = init;	
+			}
+			break;
+		case seq1:
+			state = wait;	
+			break;
+		case seq2:
+			state = init;
+			break;
+		case wait:
+			if(B1 == 0x01){
+				state = wait;
+			}
+			else if(B1 == 0x02){
+
+				state = wait;
+			}
+			else if(B1 == 0x00){
 				state = init;
-			}
-			else if(B1 && !B2){
-				state = increase;
-			}
-			else if(!B1 && B2){
-				state = decrease;
-			}
-			else{
-				state = reset;
-			}
-			
-			break;
-		case increase:
-			if(B1 && B2){
-				state = reset;
-			}
-			else{
-				state = wait2;
-			}
-			break;
-		case decrease:
-			if(B1 && B2)
-			{
-				state = reset;	
-			}
-			else{
-				state = wait2;
-			}
-			break;
+			}			
 
-		case reset:
-			if(!B1 && !B2){
-				state = init;
-			}
-			else if(B1 && !B2){
-				state = decrease;	
-			}
-			else if(B1 && B2){
-				state = reset;
-			}
 			break;
-
-
-		case wait1:
-			if(B1 && B2){
-				state = reset;
-			}
-			else if(B1 && !B2){
-				state = increase;
-			}
-			else if(!B1 && B2){
-				state = decrease;
-			}	
-			else{
-				state = wait1;
-			}	
-			break;
-		case wait2:
-			if(!B1 && !B2){
-				state = wait1;
-			}	
-			else if(B1 && B2){
-				state = reset;
-			}	
-			else{
-				state = wait2;
-			}
+		default:
 			break;
 		
 	}
 	switch(state){
+		case START:
+			break;
 		case init:
 			break;
-		case increase:
-			if(tempC < 9){
-				tempC += 0x01;
+		case seq1:
+			if(PORTC == 0x00){
+				PORTC = (PORTC << 1);
+			}
+			else{
+				PORTC = 0x01;
+			}
+			if(PORTC == 0x04){
+				PORTC = 0x38;
 			}
 			break;
-		case decrease:
-			if(tempC > 0){
-
-				tempC -= 0x01;
-			}
+		case seq2:
+			
 			break;
-		case reset:
-			tempC = 0x00;
+		case wait:
 			break;
+		default:
+			break;
+			
 	}
 	
 
@@ -129,12 +99,11 @@ int main(void) {
     /* Insert your solution below */
 
    
-    state = init;
-    tempC = 0x00;
-    //unsigned char tempC = 0x07;
+    state = START;
+ 
+  
     while (1) {
-	LED_ON();
-	PORTC = tempC;	
+	LED_ON();	
     }
     return 1;
 }
