07c0b74da4f077b18407a6850877c686a4ab9d19
choduh <choduh1995@gmail.com>
2020-05-26 19:06:15 -0700

    started midterm 2prep

---
+++ MidtermTwo/source/main.c
@@ -0,0 +1,56 @@
+/*	Author: magui070
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Midterm #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+#include "timer.h"
+
+/* SM state declarations --- fill in as needed */
+
+typedef enum output_states {OInit, Ooutput} output_states;
+typedef enum read_states {RInit, Rread} read_states;
+
+/* shared variables --- fill in as needed */
+unsigned char PA;
+
+/* state variables --- do not alter */
+output_states output_state;
+read_states read_state;
+
+/* SM definitions --- complete each task as a SM */
+#include "output.h"
+#include "read.h"
+
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+
+    // Common period for all tasks set to 100ms
+    TimerSet(100);
+    TimerOn();
+
+    // init state vars
+    
+    output_state = OInit;
+    read_state = RInit;
+    
+    /* Insert your solution below */
+    while (1) {
+	Read();
+	Output();
+	while(!TimerFlag){}
+	TimerFlag = 0;
+    }
+    return 1;
+}
