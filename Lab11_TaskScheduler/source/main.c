62089f3e093998519c539c9baeb1e067117c4d74
choduh <choduh1995@gmail.com>
2020-06-07 20:02:55 -0700

    completed part 4

--- Lab11_TaskScheduler/source/main.c
+++ Lab11_TaskScheduler/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: magui070
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #11  Exercise #3
+ *	Assignment: Lab #11  Exercise #4
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,13 +12,37 @@
 #include "timer.h"
 #include "keypad.h"
 #include "scheduler.h"
-
+#include "bit.h"
 #include "io.h"
 #include "io.c"
 #include "simAVRHeader.h"
 #endif
 
+enum STATES{start, out}lcd_state;
 
+unsigned char c;
+unsigned char x;
+
+void lcdTick(){
+
+	switch(lcd_state){
+		case start:
+			lcd_state = out;
+			break;
+		case out:
+			x = GetKeypadKey();
+			if(x!= '\0'){
+				LCD_WriteData(x);
+				if(c == 17){
+					c = 1;
+				}	
+				LCD_Cursor(c);
+				++c;
+			}
+			lcd_state = out;
+			break;
+	}	
+}
 
 int main(void) {
     /* Insert DDR and PORT initializations */
@@ -28,91 +52,28 @@ int main(void) {
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
 
-	
-	TimerSet(10);
 	TimerOn();
+	TimerSet(500);
+	
 	LCD_init();
+	LCD_DisplayString(1, "Yamero!!!");
+
+	lcd_state = start;
+	c = 1;
+	LCD_Cursor(c);
+
 	
-	LCD_WriteData('0');
 	
-	
-	unsigned char x;
 	
 	
     /* Insert your solution below */
     while (1) {
-	unsigned char keyOut = 0x00;
-	
-	x = GetKeypadKey();
 
-	switch(x){
-		case '\0':keyOut = 0x1F; break;
-		case '1': keyOut = 0x01; break;
-		case '2': keyOut = 0x02; break;
-		case '3': keyOut = 0x03; break;
-		case 'A': keyOut = 0x0A; break;
-		case '4': keyOut = 0x04; break;
-		case '5': keyOut = 0x05; break;
-		case '6': keyOut = 0x06; break;
-		case 'B': keyOut = 0x0B; break;
-		case '7': keyOut = 0x07; break;
-		case '8': keyOut = 0x08; break;
-		case '9': keyOut = 0x09; break;
-		case 'C': keyOut = 0x0C; break;
-		case '*': keyOut = 0x0E; break;
-		case '0': keyOut = 0x00; break;
-		case '#': keyOut = 0x0F; break;
-		case 'D': keyOut = 0x0D; break;
-		//default: keyOut = keyOut; break;
-		
-	}
-	
+
 	while(!TimerFlag){};
 	TimerFlag = 0;
-
-	if(keyOut == 0x01 || keyOut == 0x02 || keyOut == 0x03 || keyOut == 0x04
-		|| keyOut == 0x05 || keyOut == 0x06 || keyOut == 0x07 ||
-		keyOut == 0x08 || keyOut == 0x09 || keyOut == 0x00){
-			LCD_Cursor(1);
-			LCD_WriteData('0' + keyOut);
-		}
-	else if(keyOut == 0x0A){
-		LCD_Cursor(1);
-		LCD_WriteData('A');
-	}
-
-	else if(keyOut == 0x0B){
-		LCD_Cursor(1);
-		LCD_WriteData('B');
-	}	
-
-
-	else if(keyOut == 0x0C){
-		LCD_Cursor(1);
-		LCD_WriteData('C');
-	}
-	else if(keyOut == 0x0D){
-		LCD_Cursor(1);
-		LCD_WriteData('D');
-	}
+	lcdTick();
 	
-	else if(keyOut == 0x0E){
-		LCD_Cursor(1);
-		LCD_WriteData('*');
-	}
-	
-	else if(keyOut == 0x0F){
-		LCD_Cursor(1);
-		LCD_WriteData('#');
-	}
-	
-	else if(keyOut == 0x1F){
-		LCD_Cursor(1);
-		
-	}
-	else{
-		LCD_Cursor(1);
-	}
     }
     return 0;
 }
